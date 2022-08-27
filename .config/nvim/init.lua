50228db330fd630093c55074b0296071cb02b922
Jérémy DELGADO <jeremy.delgado@bedrockstreaming.com>
2022-08-27 21:46:04 +0200

    nvim_config: Fix undo-dir path

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -17,12 +17,12 @@ vim.cmd [[
 -- Setup undo dir
 vim.cmd [[
 if !isdirectory($HOME."/.vim")
-┊ ┊ call mkdir($HOME."/.vim", "", 0770)
+    call mkdir($HOME."/.vim", "", 0770)
 endif
-if !isdirectory($HOME."/.vim/undo-dir")
-┊ ┊ call mkdir($HOME."/.vim/undo-dir", "", 0700)
+if !isdirectory($HOME."/.config/nvim/undo-dir")
+    call mkdir($HOME."/.config/nvim/undo-dir", "", 0700)
 endif
-set undodir=~/.vim/undo-dir 
+set undodir=~/.config/nvim/undo-dir 
 set undofile
 ]]
 
