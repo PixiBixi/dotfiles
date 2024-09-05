6e725c4cf31a60f4a8508ae83a38ed0176439ef0
Jeremy Delgado <PixiBixi@users.noreply.github.com>
2024-09-05 19:03:23 +0200

    change(wezterm): add more alias

--- .wezterm.lua
+++ .wezterm.lua
@@ -77,8 +77,10 @@ config.keys = {
 
 	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
 	{key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+	{key="LeftArrow", mods="CMD", action=wezterm.action{SendString="\x1bb"}},
 	-- Make Option-Right equivalent to Alt-f; forward-word
 	{key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+	{key="RightArrow", mods="CMD", action=wezterm.action{SendString="\x1bf"}},
 
 }
 
