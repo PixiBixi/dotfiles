b8585eba9ffca6a8a7c43a12c8163c4ee04d8a6a
Jeremy Delgado <PixiBixi@users.noreply.github.com>
2026-02-21 22:11:57 +0100

    feat(wezterm): refactor config and add right status bar
    - Add clock display in right status (Tokyo Night colors)
    - Enable scrollback (10000 lines), disable audible bell
    - Reduce leader timeout to 1000ms
    - Consolidate padding, decorations and font settings
    - Clean up code style (consistent indentation, remove redundant comments)

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,122 +1,88 @@
--- Highly base on https://alexplescan.com/posts/2024/08/10/wezterm/
-
--- Import the wezterm module
 local wezterm = require 'wezterm'
-
--- Open tmux 100% window
 local mux = wezterm.mux
+
 wezterm.on("gui-startup", function(cmd)
-    local tab, pane, window = mux.spawn_window(cmd or {})
+    local _, _, window = mux.spawn_window(cmd or {})
     window:gui_window():maximize()
 end)
--- Creates a config object which we will be adding our config to
+
+-- Right status : affiche l'heure avec couleurs Tokyo Night
+wezterm.on('update-right-status', function(window, _)
+    window:set_right_status(wezterm.format {
+        { Foreground = { Color = '#565f89' } },
+        { Text = '  ' },
+        { Foreground = { Color = '#7aa2f7' } },
+        { Text = wezterm.strftime('%H:%M') },
+        { Foreground = { Color = '#565f89' } },
+        { Text = '  ' },
+    })
+end)
+
 local config = wezterm.config_builder()
 
--- (This is where our config will go)
-
--- Find them here: https://wezfurlong.org/wezterm/colorschemes/index.html
+-- Apparence
 config.color_scheme = 'Tokyo Night'
-
--- Beautiful color for active tab
 config.colors = {
     tab_bar = {
-        active_tab = {
-            fg_color = '#073642',
-            bg_color = '#2aa198',
-        }
+        active_tab = { fg_color = '#073642', bg_color = '#2aa198' }
     }
 }
-
--- Slightly transparent and blurred background
 config.window_background_opacity = 0.85
 config.macos_window_background_blur = 30
-config.window_decorations = 'RESIZE' -- disable the title bar but enable the resizable border
+config.window_decorations = 'RESIZE'
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+
+-- Fonts
 config.window_frame = {
-	font = wezterm.font({ family = 'Berkeley Mono', weight = 'Bold' }),
-	font_size = 10,
+    font = wezterm.font({ family = 'Berkeley Mono', weight = 'Bold' }),
+    font_size = 10,
 }
 
--- Leader as tmux default binding
-config.leader = { key = "b", mods = "OPT", timeout_milliseconds = 1500 }
+-- Confort
+config.audible_bell = "Disabled"
+config.scrollback_lines = 10000
+config.adjust_window_size_when_changing_font_size = false
+config.check_for_updates = false
 
-local function movePane(key, direction)
-	return {
-		key = key,
-		mods = 'LEADER',
-		action = wezterm.action.ActivatePaneDirection(direction),
-	}
-end
-local function resizePane(key, direction)
-	return {
-		key = key,
-		mods = 'CMD',
-		action = wezterm.action.AdjustPaneSize { direction, 5 }
-	}
-end
-
-
-config.keys = {
-	-- Disable default behavior for Cmd+M on MacOS
-	{
-		key = 'm',
-		mods = 'CMD',
-		action = wezterm.action.DisableDefaultAssignment,
-	},
-
-	-- Pane resize
-	resizePane('LeftArrow','Left'),
-	resizePane('RightArrow','Right'),
-	resizePane('UpArrow','Up'),
-	resizePane('DownArrow','Down'),
-
-	-- Pane move
-	movePane('LeftArrow','Left'),
-	movePane('RightArrow','Right'),
-	movePane('UpArrow','Up'),
-	movePane('DownArrow','Down'),
-
-	{
-		key = 'z',
-		mods = "LEADER",
-		action = wezterm.action.TogglePaneZoomState,
-	},
-	-- Pane split
-	{
-		mods = "LEADER",
-		key = "%",
-		action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" }
-	},
-	{
-		mods = "LEADER",
-		key = "w",
-		action = wezterm.action.CloseCurrentPane { confirm = true }
-	},
-	{
-		mods = "LEADER",
-		key = '"',
-		action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" }
-	},
-
-	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
-	{key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
-	-- Make Option-Right equivalent to Alt-f; forward-word
-	{key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
-
-}
-
+-- Tab bar
 config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
 
--- Can't use ~ or | without it
+-- Leader : OPT+b (évite conflit avec tmux Ctrl+b)
+config.leader = { key = "b", mods = "OPT", timeout_milliseconds = 1000 }
+
+-- Allows ~ | etc. with left Alt on macOS
 config.send_composed_key_when_left_alt_is_pressed = true
 
--- Disable padding for tabs
-config.window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
+
+local function movePane(key, direction)
+    return { key = key, mods = 'LEADER', action = wezterm.action.ActivatePaneDirection(direction) }
+end
+local function resizePane(key, direction)
+    return { key = key, mods = 'CMD', action = wezterm.action.AdjustPaneSize { direction, 5 } }
+end
+
+config.keys = {
+    { key = 'm', mods = 'CMD', action = wezterm.action.DisableDefaultAssignment },
+
+    resizePane('LeftArrow', 'Left'),
+    resizePane('RightArrow', 'Right'),
+    resizePane('UpArrow', 'Up'),
+    resizePane('DownArrow', 'Down'),
+
+    movePane('LeftArrow', 'Left'),
+    movePane('RightArrow', 'Right'),
+    movePane('UpArrow', 'Up'),
+    movePane('DownArrow', 'Down'),
+
+    { key = 'z', mods = 'LEADER', action = wezterm.action.TogglePaneZoomState },
+    { key = '%', mods = 'LEADER', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+    { key = '"', mods = 'LEADER', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
+    { key = 'w', mods = 'LEADER', action = wezterm.action.CloseCurrentPane { confirm = true } },
+
+    -- Word navigation (backward/forward)
+    { key = 'LeftArrow',  mods = 'OPT', action = wezterm.action.SendString("\x1bb") },
+    { key = 'RightArrow', mods = 'OPT', action = wezterm.action.SendString("\x1bf") },
 }
 
--- Returns our config to be evaluated. We must always do this at the bottom of this file
 return config
