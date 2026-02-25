3244914021d1c9dc72bad0b87c26cffc6a46dd99
Jeremy Delgado <PixiBixi@users.noreply.github.com>
2026-02-25 11:56:19 +0100

    feat(wezterm): add email hyperlink rule and simplify hyperlink_rules override

--- config/.wezterm.lua
+++ config/.wezterm.lua
@@ -85,19 +85,18 @@ config.keys = {
     { key = 'RightArrow', mods = 'OPT', action = wezterm.action.SendString("\x1bf") },
 }
 
--- Fix hyperlink detection: exclude trailing markdown bracket/paren chars
--- Default regex is too greedy and swallows )](https://...) as one URL
-local hyperlink_rules = wezterm.default_hyperlink_rules()
-for i, rule in ipairs(hyperlink_rules) do
-    if rule.regex:find('https?') then
-        -- Exclude ) ] ( [ from URL chars to avoid eating markdown badge syntax
-        hyperlink_rules[i] = {
-            regex = [=[\b(https?|ftp|file)://[^\s<>"\[\]()]*[^\s<>"\[\]()\.,;:!?'`]]=],
-            format = '$0',
-        }
-        break
-    end
-end
-config.hyperlink_rules = hyperlink_rules
+-- Override hyperlink rules to exclude trailing punctuation (e.g. trailing ')' from markdown links)
+config.hyperlink_rules = {
+    -- URLs: stop before trailing ), ], and common punctuation
+    {
+        regex = [=[\b\w+://[^\s<>"\[\]()]*[^\s<>"\[\]()\.,;:!?'`]]=],
+        format = '$0',
+    },
+    -- email addresses
+    {
+        regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
+        format = 'mailto:$0',
+    },
+}
 
 return config
